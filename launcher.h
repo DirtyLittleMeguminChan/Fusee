ef491e0eb3c26277f7146f1d256c5e304c9689f7
DirtyLittleMeguminChan <40027740+DirtyLittleMeguminChan@users.noreply.github.com>
2018-06-07 14:06:21 +0200

    Create launcher.h

---
+++ launcher.h
@@ -0,0 +1,17 @@
+#ifdef LAUNCHER_H
+#define LAUNCHER_H 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct designVariables
+{
+  float Coef_trainee;
+  float Surf_prise_air;
+  int Rayon_tuyere;
+  float Masse_vide;
+  float Volume_reservoir;
+
+}DesignVariables;
+
+#endif
